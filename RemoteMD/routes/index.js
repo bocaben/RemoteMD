a9023571659b8ad36901fdbfcd1ba2c1aa73285a
bocaben <bocaben@gmail.com>
2017-11-28 14:22:03 +0200

    robot.js gets /flashlight and echos

---
+++ RemoteMD/routes/index.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/what', function (req, res) {
+    res.render('Login', { someVar: 'some Var' });
+});
+
+module.exports = router;
---
+++ RemoteMD/routes/robot.js
@@ -0,0 +1,35 @@
+﻿// Rest API for Cloud->Robot
+
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+// Homepage
+router.get('/', function (req, res) {
+    res.render('info', { message: 'Available options: /moveXY/x/y, /move/[l|r|f|b], flashlight/[toggle|on|off]' });
+});
+
+// Flashlight - flashlight/[toggle|on|off]
+// TODO: switch to .post for production
+router.all('/flashlight/:toggleValue', function (req, res) {
+    if (['on','off','toggle'].includes(req.params.toggleValue))
+    {
+        res.json({ "flashlight post": req.params.toggleValue });
+        console.log('ROBOT: Got request to make flashlight: ' + req.params.toggleValue);
+
+    }
+    else
+        res.render('info', { message: 'Available options: flashlight/[toggle|on|off]', newtest: 'blah'});
+});
+
+//// TODO: switch to .post for production
+//router.all('/flashlight/:toggleValue', function (req, res) {
+//    if (['on', 'off', 'toggle'].includes(req.params.toggleValue)) {
+//        res.json({ "flashlight": req.params.toggleValue });
+//        console.log('ROBOT: Got request to make flashlight: ' + req.params.toggleValue);
+//    }
+//    else
+//        res.render('info', { message: 'Available options: flashlight/[toggle|on|off]' });
+//});
+
+module.exports = router;
