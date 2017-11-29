ade3da3e0266d516517bf3e20d1c166ed2314cd4
bocaben <bocaben@gmail.com>
2017-11-29 19:55:58 +0200

    trying to get index.html running through app.js

--- RemoteMD/routes/index.js
+++ RemoteMD/routes/index.js
@@ -1,10 +1,11 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index.html');
 });
 
 router.get('/what', function (req, res) {
