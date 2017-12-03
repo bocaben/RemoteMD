93c0c6194dd270987029f99828ca8ef88db22dd8
mayahamo <mayahamo@gmail.com>
2017-12-03 15:19:20 +0200

    app2.js

--- RemoteMD/app2.js
+++ RemoteMD/app2.js
@@ -2,9 +2,10 @@
 var express = require('express');
 var app = express();
 
+var routes = require('./routes/index.js');
 var cloud = require('./routes/cloud.js');
 
 app.use('/', routes);
 app.use('/cloud', cloud);
 
-var server = app.listen(80).on(error, console.log);
+var server = app.listen(80).on('error', console.log);
