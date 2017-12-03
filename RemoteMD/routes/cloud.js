95678bdb1148fc78fa2ecaddf79d2fa9f2069f59
mayahamo <mayahamo@gmail.com>
2017-12-03 14:07:41 +0200

    cloud_old.js + cloud.js

--- RemoteMD/routes/cloud.js
+++ RemoteMD/routes/cloud.js
@@ -1,31 +1,114 @@
-﻿// Rest API for Robot->Cloud
-
-'use strict';
 var express = require('express');
-var router = express.Router();
+var app = express();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
-
-router.get('/what', function (req, res) {
-    res.render('Login', { someVar: 'some Var' });
-});
-
-module.exports = router;
+/*~~~~~~ Serving static files ~~~~~~*/
+app.use(express.static(__dirname + '/views'));
 
 
-function sendJson(url, data) {
-    // Sending and receiving data in JSON format using POST method
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-            console.log(json.email + ", " + json.password);
-        }
-    };
-    xhr.send(data.stringify);
-}
+/*~~~~~~ handle GET requests ~~~~~~*/
+/*
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/' + 'index.html');
+})
+
+app.get('/doctor', function(req, res) {
+  res.sendFile(__dirname + '/views/' + 'doctor-index.html');
+})
+
+app.get('/patient', function(req, res) {
+  res.sendFile(__dirname + '/views/' + 'doctor-index.html');
+})
+
+
+app.get('/navigation', function(req, res) {
+  res.sendFile(__dirname + '/views/' + 'navigation.html');
+})
+*/
+
+/*~~~~~~ handle POST requests ~~~~~~*/
+app.post('/cloud/flashlight/on', function(req, res) {
+  fetch('ip/robot/flashlight/on', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+app.post('/cloud/flashlight/off', function(req, res) {
+  fetch('/robot/flashlight/off', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+app.post('/cloud/move/left', function(req, res) {
+  fetch('ip/robot/move/left', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+app.post('/cloud/stop', function(req, res) {
+  fetch('ip/robot/stop', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+app.post('/cloud/move', function(req, res) {
+  fetch('ip/robot/move', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+
+app.post('/cloud/move/right', function(req, res) {
+  fetch('ip/robot/move/right', { 
+		method: 'POST', 
+		headers: { "Content-type": "application/json"},
+		body: JSON.stringify()}
+	).then(response => {
+   		 if (response.ok) { return response.json(); }
+    	 throw new Error('Request failed!'); },
+	  networkError => console.log(networkError.message)
+		).then(jsonResponse => { 
+				return jsonResponse; 
+			}); 
+})
+
+
---
+++ RemoteMD/routes/cloud_old.js
@@ -0,0 +1,31 @@
+﻿// Rest API for Robot->Cloud
+
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/what', function (req, res) {
+    res.render('Login', { someVar: 'some Var' });
+});
+
+module.exports = router;
+
+
+function sendJson(url, data) {
+    // Sending and receiving data in JSON format using POST method
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.email + ", " + json.password);
+        }
+    };
+    xhr.send(data.stringify);
+}
