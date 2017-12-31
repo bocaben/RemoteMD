bf04958bf387e63573f4d356087280970c8971d2
mayahamo <mayahamo@gmail.com>
2017-12-31 16:13:29 +0200

    routes

---
+++ RemoteMD/routes/index.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var router = express.Router();
+var app = express();
+
+/* GET home page. */
+router.use('/', express.static(__dirname + '/../views'));
+
+router.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+
+module.exports = router;
